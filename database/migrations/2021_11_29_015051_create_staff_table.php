505a0c61f7b75d51d1e0755a38a415e1dcc88782
saralimbu2017 <iamsebi7@gmail.com>
2021-11-29 12:54:03 +1100

    Adding name field in create_staff_table.php migration

--- database/migrations/2021_11_29_015051_create_staff_table.php
+++ database/migrations/2021_11_29_015051_create_staff_table.php
@@ -15,6 +15,7 @@ class CreateStaffTable extends Migration
     {
         Schema::create('staff', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
             $table->timestamps();
         });
     }
